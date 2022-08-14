9de0f7c7ed41b2fa911196d1c3d020d60d280b1e
elmo2019 <elmo2019@163.com>
2022-08-15 00:17:53 +0100

    last package change

---
+++ index-codes-service/src/main/java/CJS/trend/web/IndexController.java
@@ -0,0 +1,28 @@
+package CJS.trend.web;
+
+import java.util.List;
+
+import CJS.trend.config.IpConfiguration;
+import CJS.trend.pojo.Index;
+import CJS.trend.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Autowired
+    IndexService indexService;
+    @Autowired
+    IpConfiguration ipConfiguration;
+
+//  http://127.0.0.1:8011/codes
+
+    @GetMapping("/codes")
+    @CrossOrigin
+    public List<Index> codes() throws Exception {
+        System.out.println("current instance's port is "+ ipConfiguration.getPort());
+        return indexService.get();
+    }
+}
---
+++ index-gather-store-service/src/main/java/CJS/trend/job/IndexDataSyncJob.java
@@ -0,0 +1,36 @@
+package CJS.trend.job;
+
+import java.util.List;
+
+import cn.hutool.core.date.DateUtil;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import CJS.trend.pojo.Index;
+import CJS.trend.service.IndexDataService;
+import CJS.trend.service.IndexService;
+
+public class IndexDataSyncJob extends QuartzJobBean {
+
+    @Autowired
+    private IndexService indexService;
+
+    @Autowired
+    private IndexDataService indexDataService;
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        System.out.println("定时启动：" + DateUtil.now());
+        List<Index> indexes = indexService.fresh();
+        for (Index index : indexes) {
+            indexDataService.fresh(index.getCode());
+        }
+        System.out.println("定时结束：" + DateUtil.now());
+
+    }
+
+}
+
+
